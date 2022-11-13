97b01c02abea9fb53500320833ea41cae2e5452f
Diemandan <Diemandan63@gmail.com>
2022-11-13 17:46:40 +0100

    add dump of database and task in cron

---
+++ app/Console/Commands/DbDump.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DbDump extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'notepad:dump';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'make dump of your dataBase to: storage/db-dumps';
+
+    
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    
+    public function handle()
+    {
+        $mysqldump='  mysqldump -u admin  -padmin notepad > /var/www/notepad/storage/db-dumps/notepad_'.date('h:i:s,d.m.y').'.sql';
+        $output = array();
+        exec ( $mysqldump,$output ) ;
+   
+    }
+}
+
